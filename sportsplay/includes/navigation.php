c7ab508a1caab269b43f8722da664b7f7f2ba628
Fran051203 <ff3468@g.rit.edu>
2026-02-14 20:46:57 +0100

    Navigation
    Navigation and footer files ready for implementation

---
+++ sportsplay/includes/navigation.php
@@ -0,0 +1,30 @@
+<header class="navbar">
+
+    <div class="nav-left">
+        <img src="../assets/images/sportsplay-logo.png" alt="SportsPlay Logo" class="logo">
+    </div>
+
+    <nav class="nav-center">
+        <a href="#" class="nav-item active">HOME</a>
+        <a href="#" class="nav-item">TEAMS</a>
+        <a href="#" class="nav-item">COACH</a>
+        <a href="#" class="nav-item">ENROLL</a>
+    </nav>
+
+    <div class="nav-right">
+        
+        <div class="search-container">
+            <span class="search-icon">&#128269;</span>
+            <div class="search-dropdown">
+                <input type="text" placeholder="Search...">
+            </div>
+        </div>
+
+        <a href="#" class="login-btn">LOGIN</a>
+
+        <div class="menu-icon">
+            ☰
+        </div>
+    </div>
+
+</header>
